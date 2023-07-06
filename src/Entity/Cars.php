33b387b50929a07b9e736d0fdf3fdbbfb135576a
NaimC <naim.chihi@gmail.com>
2023-07-06 12:19:09 +0200

    database and entity created

---
+++ src/Entity/Cars.php
@@ -0,0 +1,96 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\CarsRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: CarsRepository::class)]
+class Cars
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $Brand = null;
+
+    #[ORM\Column]
+    private ?float $price = null;
+
+    #[ORM\Column(type: Types::DATE_MUTABLE)]
+    private ?\DateTimeInterface $year = null;
+
+    #[ORM\Column]
+    private ?int $km = null;
+
+    #[ORM\Column(type: Types::TEXT)]
+    private ?string $characteristic = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getBrand(): ?string
+    {
+        return $this->Brand;
+    }
+
+    public function setBrand(string $Brand): static
+    {
+        $this->Brand = $Brand;
+
+        return $this;
+    }
+
+    public function getPrice(): ?float
+    {
+        return $this->price;
+    }
+
+    public function setPrice(float $price): static
+    {
+        $this->price = $price;
+
+        return $this;
+    }
+
+    public function getYear(): ?\DateTimeInterface
+    {
+        return $this->year;
+    }
+
+    public function setYear(\DateTimeInterface $year): static
+    {
+        $this->year = $year;
+
+        return $this;
+    }
+
+    public function getKm(): ?int
+    {
+        return $this->km;
+    }
+
+    public function setKm(int $km): static
+    {
+        $this->km = $km;
+
+        return $this;
+    }
+
+    public function getCharacteristic(): ?string
+    {
+        return $this->characteristic;
+    }
+
+    public function setCharacteristic(string $characteristic): static
+    {
+        $this->characteristic = $characteristic;
+
+        return $this;
+    }
+}
