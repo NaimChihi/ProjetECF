3d970bc43ba41b45b57ea50a5941231f9428e140
NaimC <naim.chihi@gmail.com>
2023-07-10 16:09:27 +0200

    adminController

---
+++ src/Controller/AdminController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\User;
+use App\Form\EditUserType;
+use App\Repository\UserRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+
+#[Route('/admin', name: 'admin_')]
+
+class AdminController extends AbstractController
+{
+    #[Route('/', name: 'index')]
+    public function index(): Response
+    {
+        return $this->render('admin/index.html.twig', [
+            'controller_name' => 'AdminController',
+        ]);
+    }
+
+    /**
+     * Liste des utilisateurs du site
+     * 
+     */
+    
+    #[Route('/utilisateurs', name: 'utilisateurs')]
+    public function usersList (UserRepository $users) {
+        return $this->render("admin/users.html.twig", [
+            'users' => $users->findAll()
+        ]);
+
+    }
+
+    /**
+     * Modifier un utilisateur
+     */
+
+    #[Route('/utilisateurs/modifier/{id}', name:'modifier_utilisateur')]
+
+    public function editUser(User $user, Request $request, EntityManagerInterface $entityManager):Response
+    {
+        $form = $this->createForm(EditUserType::class, $user);
+        $form ->handleRequest($request);
+    
+        
+        if ($form ->isSubmitted() && $form-> isValid()){
+            $entityManager->persist($user);
+            $entityManager->flush();
+        
+
+            $this->addFlash('message', 'Utilisateur modifié avec succès');
+            return $this->redirectToRoute('admin_utilisateurs');
+        }
+            return $this->render('admin/edituser.html.twig', [
+                'userForm' => $form->createView()
+            ]);
+        }
+    }
+
+
