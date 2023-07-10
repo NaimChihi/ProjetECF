78b20dc61b802a42c381e782bef8a7d78c9d7a57
NaimC <naim.chihi@gmail.com>
2023-07-10 22:49:03 +0200

    templates

---
+++ public/js/script.js
@@ -0,0 +1,31 @@
+let playOnce = true;
+
+window.addEventListener("scroll", () => {
+  //navbar effect
+if (window.scrollY > 50) {
+    navbar.style.height = "45px";
+} else {
+    navbar.style.heignt = "90px";
+}
+
+  //image
+let scrollvalue =
+    (window.scrollY + window.innerHeight) / document.body.offsetHeight;
+if (scrollvalue > 0.45) {
+    imgImprovise.style.opacity = 1;
+    imgImprovise.style.transform = "none";
+}
+
+  //popup
+if (scrollvalue > 0.85 && playOnce) {
+    popup.style.opacity = 1;
+    popup.style.transform = "none";
+    playOnce = false;
+}
+
+  //evenement au clic (fermer la popup)
+closeBtn.addEventListener("click", () => {
+    popup.style.opacity = 0;
+    popup.style.transform = "translateX(500px)";
+});
+});
